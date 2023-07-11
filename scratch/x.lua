a70d44372c464eeb212aa819838e0eea15581fa1
haoliang <haoliang0.1.2@gmail.com>
2023-07-11 19:07:42 +0800

    wip: MVP

---
+++ scratch/x.lua
@@ -0,0 +1,26 @@
+local x = [[
+hello
+]]
+
+print(x)
+
+do
+end
+
+for i = 1, 3 do
+end
+
+do
+end
+
+do
+  print("hello")
+end
+
+do
+  print("hello" .. "world")
+end
+
+print('hello')
+
+local x = s .. 'hello'
